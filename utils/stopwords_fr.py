38d6784be8d797ef3c5068835eb239ec5b16db19
austin <67250549+xtx83@users.noreply.github.com>
2025-06-30 00:04:16 -0400

    Create stopwords_fr.py

---
+++ utils/stopwords_fr.py
@@ -0,0 +1,16 @@
+# utils/stopwords_fr.py
+
+
+STOPWORDS_FR = {
+    "je", "tu", "il", "elle", "nous", "vous", "ils", "elles",
+    "me", "te", "se", "le", "la", "les", "en", "y",
+    "de", "du", "des", "au", "aux", "à", "dans", "sur", "chez", "avec", "sans",
+    "ce", "cet", "cette", "ces", "mon", "ton", "son", "notre", "votre", "leur",
+    "et", "mais", "ou", "donc", "ni",
+    "un", "une", "le", "la", "les", "du", "de", "des",
+    "ne", "pas", "très", "trop", "bien", "mal", "aussi", "encore", "toujours",
+    "être", "avoir", "faire", "aller", "venir",
+    "c'est", "il y a", "ça", "ce", "qui", "que", "où",
+    "comment", "pourquoi", "parce", "quand", "si",
+    "oui", "non", "peut", "doit", "faut", "va"
+}
